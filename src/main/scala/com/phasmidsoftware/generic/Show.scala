8297b47dd0298173802ad69487c5ba9b40d3d92e
Robin Hillyard <scalaprof@gmail.com>
2019-06-10 18:09:56 -0400

    Add derived files.

--- src/main/scala/com/phasmidsoftware/generic/Show.scala
+++ src/main/scala/com/phasmidsoftware/generic/Show.scala
@@ -1,5 +1,17 @@
 package com.phasmidsoftware.generic
 
-trait Show {
-
-}
+//trait Show[T] extends Numeric[T] {
+//  def show(): Unit
+//}
+//
+//trait Collection[T] {
+//  this: { def show(): Unit } =>
+//}
+//
+//object Main extends App {
+//  case  class ShowableInt(x: Int) {
+//    def show(): Unit = println(x)
+//  }
+//
+//  class ShowableCollection extends
+//}
